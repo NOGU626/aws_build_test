fac103f422b49c5612232b91da2263cce8db23ba
野口大輝 <noguchi@cps.im.dendai.ac.jp>
2020-09-03 09:20:24 +0900

    first commit2

--- main.py
+++ main.py
@@ -8,21 +8,22 @@ FILE_CONTENTS = ""
 
 # Timer
 MAX_ITER = 10
-SEC = 60
+SEC = 20
 
 def PutS3(i):
-  s3 = boto3.resource('s3')
-  bucket = s3.Bucket(BUCKET_NAME)
-  filename = str(i+1)
-
-  obj = bucket.put_object(ACL='private', Body=FILE_CONTENTS, Key=TARGET_DIR + "/" + filename, ContentType='text/plain')
-  return str(obj)
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(BUCKET_NAME)
+    bucket.create()
+    filename = str(i+1)
+    
+    obj = bucket.put_object(ACL='private', Body=FILE_CONTENTS, Key=TARGET_DIR + "/" + filename, ContentType='text/plain')
+    return str(obj)
 
 def count(i):
-  print("{}秒経過しました。".format((i+1)*SEC))
+    print("{}秒経過しました。".format((i+1)*SEC))
 
 if __name__ == '__main__':
-  for i in range(MAX_ITER):
+    for i in range(MAX_ITER):
     sleep(SEC)
     count(i)
     PutS3(i)
